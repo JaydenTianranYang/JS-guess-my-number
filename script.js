1cdb140ea0492e08205b1c62b5c4496cf315db66
Jayden Yang <jayden.yang@rea-group.com>
2025-03-11 23:04:21 +1100

    Add addEventListener function

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 'use strict';
 
 // document.querySelector('.message');
-document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!';
+    }
+});
