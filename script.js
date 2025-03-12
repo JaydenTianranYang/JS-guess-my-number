0c5c70806cabc03034b3a086bf12a1fc10d554e4
jyzhang660 <jyzhang660@163.com>
2025-03-12 22:10:36 +1100

    Add `You lost the game` logic

--- script.js
+++ script.js
@@ -13,20 +13,29 @@ let score = 20;
 
 document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click'),
+  function () {
+    const guess = Number(document.querySelector('.guess').value);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = ' Too High!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = ' Too Low!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  }
-});
+    if (!guess) {
+      document.querySelector('.message').textContent = '⛔ No Number!';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = '🎉 Correct Number!';
+    } else if (guess > secretNumber) {
+      if (score > 0) {
+        document.querySelector('.message').textContent = ' Too High!';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You lost the game!';
+      }
+    } else if (guess < secretNumber) {
+      if (score > 0) {
+        document.querySelector('.message').textContent = ' Too Low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You lost the game!';
+      }
+    }
+  };
